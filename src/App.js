94666593998737f0032e4c57acd7f5d6c403842e
Danne <dannepi.dev@gmail.com>
2022-09-18 02:39:26 -0300

    feat: Estruturação inicial do projeto.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from './components/NavBar.js'
+import Body from './components/Body.js'
+import './styles/Body.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sugestions.js
@@ -0,0 +1,5 @@
+export default function Sugestions(){
+    return (
+        <div>Sugestions</div>
+    )
+}
